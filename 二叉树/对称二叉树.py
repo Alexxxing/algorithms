84657fcfd2aaa772d469c2ad6e6e26d47774a933
Alexwang <alexwang@canway.net>
2024-12-03 18:14:33 +0800

    feature: symmetric tree judge

--- 二叉树/对称二叉树.py
+++ 二叉树/对称二叉树.py
@@ -4,14 +4,22 @@ from 二叉树.base_tree import TreeNode
 
 
 def is_symmetric(root: Optional[TreeNode]) -> bool:
-    if not root:
-        return
 
-    return is_symmetric(root.left) == is_symmetric(root.right)
+    def recur(left, right):
+        if not left and not right:
+            return True
+        if not left or not right:
+            return False
+        if left.val != right.val:
+            return False
+
+        return recur(left.left, right.right) and recur(left.right, right.left)
+
+    return recur(root.left, root.right)
 
 
 if __name__ == '__main__':
     tree = TreeNode(0)
     tree.left = TreeNode(1)
     tree.right = TreeNode(2)
-    assert (is_symmetric(tree), False)
+    assert (is_symmetric(tree) is False)
