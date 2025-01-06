93199f6f6f6deb8de3b5ba00f7550a1436fcf669
Alexwang <648722526@qq.com>
2025-01-06 14:17:48 +0800

    minor: length of longest substring

--- 滑动窗口/无重复字符的最长子串.py
+++ 滑动窗口/无重复字符的最长子串.py
@@ -1,14 +1,31 @@
 def length_of_longest_substring(s: str) -> int:
-    dic, left, ans = {}, -1, 0
-    for i in range(len(s)):
-        if s[i] in dic:
-            left = max(dic[s[i]], left)
-        dic[s[i]] = i
-        ans = max(ans, i - left)
-
+#     dic, left, ans = {}, -1, 0
+#     for i in range(len(s)):
+#         if s[i] in dic:
+#             left = max(dic[s[i]], left)
+#         dic[s[i]] = i
+#         ans = max(ans, i - left)
+#
+#     return ans
+    left = 0
+    right = 0
+    n = len(s)
+    if n == 1:
+        return n
+    ans = 0
+    dict_map = {}
+    while right < n:
+        if s[right] in dict_map:
+            print(ans)
+            left = max(left, dict_map[s[right]] + 1)
+            dict_map[s[right]] = right
+        else:
+            dict_map[s[right]] = right
+        ans = max(ans, right - left + 1)
+        right += 1
     return ans
 
 
 if __name__ == '__main__':
-    s = "pwwkew"
+    s = "nfpdmpi"
     print(length_of_longest_substring(s))
