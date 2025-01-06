4bc098623c8be7e8864d7f8b518bdfc4487b92b5
Alexwang <648722526@qq.com>
2025-01-06 14:18:07 +0800

    minor: build a quadtree

--- 分治法/建立四叉树.py
+++ 分治法/建立四叉树.py
@@ -12,36 +12,34 @@ class Node:
 
 
 def construct(grid: List[List[int]]) -> 'Node':
-    m, n = len(grid), (len(grid[0]) if grid else 0)
-    total = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
-    for i in range(1, m + 1):
+    n = len(grid)
+    total = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+    for i in range(1, n + 1):
         for j in range(1, n + 1):
-            total[i][j] = total[i][j - 1] + total[i - 1][j] - total[i - 1][j - 1] + grid[i -1][j -1]
+            total[i][j] = total[i][j - 1] + total[i - 1][j] - total[i - 1][j - 1] + grid[i - 1][j - 1]
 
-    def get_sum(r0, c0, r1, c1):
-        res = total[r1][c1] - total[r1][c0] - total[r0][c1] + total[r0][c0]
-        return res
+    def sum_total(r1, r0, c1, c0):
+        return total[r1][c1] - total[r1][c0] - total[r0][c1] + total[r0][c0]
 
-    def recur(r0, c0, r1, c1):
-        sums = get_sum(r0, c0, r1, c1)
-        if sums == 0:
+    def recur(r1, r0, c1, c0):
+        s = sum_total(r1, r0, c1, c0)
+        if s == 0:
             return Node(False, True)
-        if sums == (r1 - r0 if (r1 - r0) else 1) * (c1 - c0 if (c1 - c0) else 1):
+        if s == (r1 - r0) * (c1 - c0):
             return Node(True, True)
-
-        node = Node(
+        return Node(
             True,
             False,
-            recur(r0, c0, (r0 + r1) // 2, (c0 + c1) // 2),
-            recur(r0, (c0 + c1) // 2, (r0 + r1) // 2, c1),
-            recur((r0 + r1) // 2, c0, r1, (c0 + c1) // 2),
-            recur((r0 + r1) // 2, (c0 + c1) // 2, r1, c1),
+            recur((r0 + r1) // 2, r0, (c0 + c1) // 2, c0),
+            recur((r0 + r1) // 2, r0, c1, (c0 + c1) // 2),
+            recur(r1, (r0 + r1) // 2, (c0 + c1) // 2, c0),
+            recur(r1, (r0 + r1) // 2, c1, (c0 + c1) // 2)
         )
-        return node
 
-    return recur(0, 0, len(grid), len(grid[0]))
+    return recur(n, 0, n, 0)
 
 
 if __name__ == '__main__':
     grid = [[0, 1], [1, 0]]
-    print(construct(grid))
+    tree = construct(grid)
+    print(tree)
