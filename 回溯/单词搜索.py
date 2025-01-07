0da81893665e41cdff779a516944b0b924e4cf3b
Alexwang <648722526@qq.com>
2025-01-07 13:58:25 +0800

    minor: word exist search

--- 回溯/单词搜索.py
+++ 回溯/单词搜索.py
@@ -1,29 +1,38 @@
+"""
+给定一个 m x n 二维字符网格 board 和一个字符串单词 word 。如果 word 存在于网格中，返回 true ；否则，返回 false 。
+
+单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+
+
+"""
+
+
 def exist(board, word):
     """
     :type board: List[List[str]]
     :type word: str
     :rtype: bool
     """
-
     m, n = len(board), len(board[0])
 
-    def dfs(r, c, k):
-        if not 0 <= r < m or not 0 <= c < n or board[r][c] != word[k]:
-            return False
+    def in_area(i, j):
+        return bool(0 <= i < m and 0 <= j < n)
 
-        if k == len(word) - 1:
+    def recur(i, r, c):
+        if not in_area(r, c) or board[r][c] != word[i]:
+            return False
+        if i == len(word) - 1:
             return True
 
         board[r][c] = ""
-        res = dfs(r + 1, c, k + 1) or dfs(r - 1, c, k + 1) or dfs(r, c + 1, k + 1) or dfs(r, c - 1, k + 1)
-        board[r][c] = word[k]
+        res = recur(i + 1, r - 1, c) or recur(i + 1, r + 1, c) or recur(i + 1, r, c - 1) or recur(i + 1, r, c + 1)
+        board[r][c] = word[i]
         return res
 
-    for r in range(m):
-        for c in range(n):
-            if board[r][c] == word[0]:
-                if dfs(r, c, 0):
-                    return True
+    for i in range(m):
+        for j in range(n):
+            if recur(0, i, j):
+                return True
     return False
 
 
