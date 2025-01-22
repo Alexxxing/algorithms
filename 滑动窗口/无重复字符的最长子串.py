f56cf153b9ce08031ed4b644c8602cd1199f2fa3
Alexwang <648722526@qq.com>
2025-01-22 19:49:20 +0800

    feature: length of longest substring

--- 滑动窗口/无重复字符的最长子串.py
+++ 滑动窗口/无重复字符的最长子串.py
@@ -1,31 +1,40 @@
+"""
+给定一个字符串 s ，请你找出其中不含有重复字符的 最长
+子串
+ 的长度。
+
+示例 1:
+
+输入: s = "abcabcbb"
+输出: 3
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+
+示例 2:
+
+输入: s = "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+输入: s = "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+"""
+
+
 def length_of_longest_substring(s: str) -> int:
-#     dic, left, ans = {}, -1, 0
-#     for i in range(len(s)):
-#         if s[i] in dic:
-#             left = max(dic[s[i]], left)
-#         dic[s[i]] = i
-#         ans = max(ans, i - left)
-#
-#     return ans
-    left = 0
-    right = 0
-    n = len(s)
-    if n == 1:
-        return n
+    dic = {}
+    l = -1
     ans = 0
-    dict_map = {}
-    while right < n:
-        if s[right] in dict_map:
-            print(ans)
-            left = max(left, dict_map[s[right]] + 1)
-            dict_map[s[right]] = right
-        else:
-            dict_map[s[right]] = right
-        ans = max(ans, right - left + 1)
-        right += 1
+    for i in range(len(s)):
+        if s[i] in dic:
+            l = max(dic[s[i]], l)
+        dic[s[i]] = i
+        ans = max(ans, i - l)
     return ans
 
 
 if __name__ == '__main__':
-    s = "nfpdmpi"
+    s = "au"
     print(length_of_longest_substring(s))
