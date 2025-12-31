a1743d3e1f1817f7dc6207ee52bc61bbb92291b9
Alexxxing <648722526@qq.com>
2025-12-31 17:15:48 +0800

    minor: 加油站优化

--- 双指针/加油站.py
+++ 双指针/加油站.py
@@ -1,11 +1,12 @@
+from typing import List
+
+
 """
 在一条环路上有 n 个加油站，其中第 i 个加油站有汽油 gas[i] 升。
 
 你有一辆油箱容量无限的的汽车，从第 i 个加油站开往第 i+1 个加油站需要消耗汽油 cost[i] 升。你从其中的一个加油站出发，开始时油箱为空。
 
 给定两个整数数组 gas 和 cost ，如果你可以按顺序绕环路行驶一周，则返回出发时加油站的编号，否则返回 -1 。如果存在解，则 保证 它是 唯一 的。
-
-
 
 示例 1:
 
@@ -20,26 +21,19 @@
 开往 3 号加油站，你需要消耗 5 升汽油，正好足够你返回到 3 号加油站。
 因此，3 可为起始索引。
 """
-from typing import List
 
 
 def can_complete_circuit(gas: List[int], cost: List[int]) -> int:
-    n = len(gas)
+    if sum(gas) < sum(cost):
+        return -1
+    gas_sum = 0
     ans = 0
-    while ans < n:
-        g_t, c_t = 0, 0
-        tag = 0
-        while tag < n:
-            t = (ans + tag) % n
-            g_t += gas[t]
-            c_t += cost[t]
-            if c_t > g_t:
-                break
-            tag += 1
-        if tag == n:
-            return ans
-        ans += 1
-    return -1
+    for i in range(len(gas)):
+        gas_sum += gas[i] - cost[i]
+        if gas_sum < 0:
+            ans = i + 1
+            gas_sum = 0
+    return ans
 
 
 if __name__ == '__main__':
